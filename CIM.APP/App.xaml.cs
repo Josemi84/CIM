76bdca8964ef093c3296d4ad96f6880aad214ad7
Jocava96 <josemicasadovalle@hotmail.com>
2025-06-02 20:39:13 +0200

    Agregar archivos de proyecto.

---
+++ CIM.APP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CIM.APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
